877b6f5630fd3a30a0772e42ecf33930368f6ee3
Zevankai <langekyl@gmail.com>
2025-11-11 21:12:08 +0000

    Add explicit path for publicDir

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import path from 'path'; // <-- ADD THIS IMPORT
 
 export default defineConfig({
   base: "/AdvancedInventory/",
-  publicDir: 'public', // <-- ADD THIS LINE
+  
+  // This line is the new, robust fix
+  publicDir: path.resolve(__dirname, 'public'), 
+  
   server: {
     cors: {
       origin: "https://www.owlbear.rodeo",
