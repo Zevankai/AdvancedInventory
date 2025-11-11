e262d3e330d1c6d45845a5a7f3f0de595d00700e
Zevankai <langekyl@gmail.com>
2025-11-11 15:20:36 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
-import path from 'path'; // <-- ADD THIS IMPORT
 
 export default defineConfig({
   base: "/AdvancedInventory/",
-  
-  // This line is the new, robust fix
-  publicDir: path.resolve(__dirname, 'public'), 
-  
   server: {
     cors: {
       origin: "https://www.owlbear.rodeo",
